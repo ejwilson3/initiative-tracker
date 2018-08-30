1b5a9ba059c682756a9c37ea9b547912e53d97cf
Ellis the Wilson <ejwilson3@wisc.edu>
2018-08-30 18:24:50 -0500

    Better format, slightly nicer

--- tracker.py
+++ tracker.py
@@ -1,12 +1,13 @@
 import random
 class _Combatant():
     def __init__(self):
-        self.initiative = 0
-        self.PC = False
-        self.name = ""
+        self.AC = 0
+        self.HP = 0
 
 def spill(fighter):
-    print(fighter.name, fighter.initiative)
+    fmt = "{id:d}\t{init:d}\t{name:s}\t\t{HP:d}\t{AC:d}"
+    print(fmt.format(id=fighter.idx, init=fighter.initiative, name=fighter.name,
+                     HP=fighter.HP, AC = fighter.AC))
     return
 
 def new_fighter():
@@ -16,9 +17,9 @@ def new_fighter():
         NPC = input("NPC or PC? ")
     newFighter.PC = (NPC ==  "PC")
     newFighter.name = input("Name: ")
-    modifier = input("Modifier: ")
-    #modifier = modifer
-    newFighter.initiative = random.randrange(1, 21) + int(modifier)
+    newFighter.modifier = int(input("Modifier: "))
+    #newFighter.modifier = modifer
+    newFighter.initiative = random.randrange(1, 21) + newFighter.modifier
     return newFighter
 
 combatants = []
@@ -27,6 +28,8 @@ running = True
 while running:
     
     if combatants != []:
+        stats = "id\tinit\tname\t\tHP\tAC"
+        print(stats)
         for fighter in combatants:
             spill(fighter)
     print("q = quit, n = new, r = reroll, e = edit, l = load, s = Save")
@@ -37,7 +40,14 @@ while running:
 
     if command == "n":
         newFighter = new_fighter()
+        newFighter.idx = len(combatants)
         combatants.append(newFighter)
         combatants.sort(key=lambda x: x.initiative, reverse=True)
+
+    if command == "r":
+        PCs = input("PCs? ") in ["y", "yes"]
+        for fighter in combatants:
+            if PCs or not fighter.PC:
+                fighter.initiative = random.randrange(1, 21) + fighter.modifier
 
     print("")
